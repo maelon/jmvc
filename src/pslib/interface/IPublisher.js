3fa9d8a8c73fd62e9069bf88cadbe5d9c40e66c9
maelon <maelon.j@gmail.com>
2016-04-13 22:37:42 +0800

    backup

---
+++ src/pslib/interface/IPublisher.js
@@ -0,0 +1,8 @@
+/*===================================================================
+#    FileName: IPublisher.js
+#      Author: Maelon.J
+#       Email: maelon.j@gmail.com
+#  CreateTime: 2016-04-13 21:42
+# Description: interface for publisher
+===================================================================*/
+
---
+++ src/pslib/interface/ISubscriber.js
@@ -0,0 +1,42 @@
+/*===================================================================
+#    FileName: ISubscriber.js
+#      Author: Maelon.J
+#       Email: maelon.j@gmail.com
+#  CreateTime: 2016-04-13 21:42
+# Description: 订阅者的抽象类
+===================================================================*/
+
+class ISubscriber {
+
+    /**
+    * @description 订阅者的抽象类
+    * @constructor
+    */
+    constructor() {
+    }
+
+    /**
+    * @description 设置订阅通知的回调方法
+    * @public
+    * @param {Function} method 通知回调的方法
+    */
+    set notifyMethod(method) {
+    }
+
+    /**
+    * @description 设置订阅通知的回调方法上下文
+    * @public
+    * @param {Object} context 回调方法上下文
+    */
+    set notifyContext(context) {
+    }
+
+    /**
+    * @description 通知订阅者
+    * @public
+    * @param {INotifation} notification 通知对象
+    */
+    notifySubscriber(notification) {
+    }
+}
+
