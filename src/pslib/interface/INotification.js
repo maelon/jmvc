20e83375d12ad600e60b44035ecd9599ba86698e
MaelonJinG <maelon@Maelon.lan>
2016-04-15 20:44:58 +0800

    add INotification

---
+++ src/pslib/interface/INotification.js
@@ -0,0 +1,41 @@
+/*===================================================================
+#    FileName: INotification.js
+#      Author: Maelon.J
+#       Email: maelon.j@gmail.com
+#  CreateTime: 2016-04-14 11:14
+# Description: 通知抽象类
+===================================================================*/
+
+class INotification {
+
+    /**
+    * @description
+    * @constructor
+    */
+    constructor() {
+    }
+
+    /**
+    * @description
+    * @public
+    * @return 
+    */
+    get name() {
+    }
+
+    /**
+    * @description
+    * @public
+    * @return 
+    */
+    get body() {
+    }
+
+    /**
+    * @description
+    * @public
+    * @return 
+    */
+    get type() {
+    }
+}
--- src/pslib/interface/ISubscriber.js
+++ src/pslib/interface/ISubscriber.js
@@ -40,3 +40,4 @@ class ISubscriber {
     }
 }
 
+export default ISubscriber;
